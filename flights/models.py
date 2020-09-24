86a48cc950872d01c84d4d3466c2a246a8c4c96a
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-09-24 08:18:17 +0300

    Added Airport db model

--- flights/models.py
+++ flights/models.py
@@ -9,3 +9,10 @@ class Flight(models.Model):
     # This is to return formated string when querying db
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}, duration {self.duration}min"
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
