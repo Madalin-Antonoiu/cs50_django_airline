c0e7289af2008bf17c86ce7abf70988daead8c1e
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-09-24 11:18:14 +0300

    💡 Django Admin interface with list view tables!

--- flights/admin.py
+++ flights/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import  Airport, Flight, Passenger
 
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ["id", "origin", "destination", "duration"]
+
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ["id", "code", "city"]
+
+class PassengerAdmin(admin.ModelAdmin):
+    list_display = ["id", "first", "last"]
+
 # Register your models here.
-admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
+admin.site.register(Airport, AirportAdmin)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
